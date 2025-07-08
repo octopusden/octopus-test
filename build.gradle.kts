68a6d6a82b06c6525fbeb123a18e91bda7899083
Octopusden Root <51118814+octopusden@users.noreply.github.com>
2025-07-08 12:34:22 +0200

    Update sonatype coordinates for gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,8 +27,8 @@ java {
 nexusPublishing {
     repositories {
         sonatype {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
             username.set(System.getenv("MAVEN_USERNAME"))
             password.set(System.getenv("MAVEN_PASSWORD"))
         }
